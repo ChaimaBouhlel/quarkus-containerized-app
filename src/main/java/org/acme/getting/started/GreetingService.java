fe18b6b550d5adccc69883cedee3f9e2834cd19a
ChaimaBouhlel <chaimabouhlel10@gmail.com>
2024-10-18 00:44:47 +0100

    add comment to test

--- src/main/java/org/acme/getting/started/GreetingService.java
+++ src/main/java/org/acme/getting/started/GreetingService.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class GreetingService {
 
+    //new comment
     public String greeting(String name) {
         return "hello " + name;
     }
